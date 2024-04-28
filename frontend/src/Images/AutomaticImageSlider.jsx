1d881ad46382c8faa42bf26510f1a499b91a0821
anirudhkala110 <anirudhkala110@gmail.com>
2024-04-28 16:05:38 +0530

    Updated Image Slider

--- frontend/src/Images/AutomaticImageSlider.jsx
+++ frontend/src/Images/AutomaticImageSlider.jsx
@@ -9,14 +9,14 @@ import JoinUs from './JoinUs.jpg'
 import mi from './MI.svg'
 import one from './1.png'
 import two from './2T.png'
-import tre from './3.png'
-import fr from './4.jpg'
+import SLS from './SLS.png'
+import SSL from './SSL.jpg'
 import fv from './frst.png'
 import six from './6.jpg'
 import svn from './7.jpg'
 import eth from './8.jpg'
-import nin from './9.jpg'
-import elv from './11.jpg'
+import SSAE from './SSAE.png'
+import SSE from './SSE.jpg'
 import tlv from './12.jpg'
 import trln from './13.jpg'
 import frtn from './14.jpg'
@@ -29,7 +29,9 @@ import 'aos/dist/aos.css';
 
 const colors = [
     { img: fv, info: "Complete Home Automation Using Matter Protocols" },
+    { img: SSL, info: "Smart Lights and Switches Using Matter Protocols" },
     { img: six, info: 'Smart Display Control' },
+    { img: SLS, info: 'Smart Light and Switches, controled by one click' },
     { img: svn, info: 'Control AC, Fans from Anywhere' },
     { img: eth, info: 'Get the Latest Update of Security' },
     { img: tlv, info: 'Control Your Home System Through Voice' },
@@ -37,6 +39,8 @@ const colors = [
     { img: frtn, info: 'System for automate washing Machines' },
     { img: sxtn, info: 'Control and Automate Home From your Smart Phone' },
     { img: svtn, info: 'Get Video Updates on Smart Devices' },
+    { img: SSAE, info: 'Smart Emergency Monitoring, Voice controled and Automated by Matter Protocol' },
+    { img: SSE, info: 'Smart Security and Monitoring, controled and Automated by Matter Protocol' },
     { img: Intro, info: 'Get room Temperature Updates and Set your Fan to maintain a fix Room Temperature on your Demand' },
     { img: Navigation, info: 'Control Your Complete Home Devices by single touch' },
     { img: mi, info: ' Automation Your Home Using Matter Protocol' }
@@ -107,7 +111,7 @@ function AutomaticImageSlider() {
                     >
                         <center className='w-100 text-white alert bg-dark rounded-0'>{data.info}</center>
                         {
-                            <img className=' w-100 text-white bg-dark shadow-lg' src={data.img} style={{ maxHeight: '450px', height: '100%', minWidth: '400px', maxWidth: "", width: '-webkit-fill-available', color: 'white' }} /> ? <div className='bg-dark'><img className=' w-100 text-white py-2' src={data.img} style={{ color: "white", maxHeight: '450px',filter:`${data.img==mi?'invert(1)':'invert(0)'}`, height: '100%', minWidth: '400px', maxWidth: "", width: '-webkit-fill-available', color: 'white' }} /></div> : 'No More Images'
+                            <img className=' w-100 text-white bg-dark shadow-lg' src={data.img} style={{ maxHeight: '450px', height: '100%', minWidth: '400px', maxWidth: "", width: '-webkit-fill-available', color: 'white' }} /> ? <div className='bg-dark' style={{ color: "white", maxHeight: '450px', height: '100%', minWidth: '400px', maxWidth: "", width: '-webkit-fill-available', color: 'white' }}><img className=' w-100 text-white py-2' src={data.img} style={{ color: "white", maxHeight: '450px',filter:`${data.img==mi?'invert(1)':'invert(0)'}`, height: '100%', minWidth: '400px', maxWidth: "", width: '-webkit-fill-available', color: 'white' }} /></div> : 'No More Images'
                         }
                         {/* data-aos="slide-left" data-aos-delay={1000*idx} data-aos-anchor-easing='ease-in' data-aos-duration='1000' data-aos-mirror='true'/> */}
                     </div>
