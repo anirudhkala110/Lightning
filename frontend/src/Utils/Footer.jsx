d2e3890796dbe2b5c868e8384e5956bc74026613
anirudhkala110 <anirudhkala110@gmail.com>
2024-04-29 16:00:50 +0530

    Updated new Content

--- frontend/src/Utils/Footer.jsx
+++ frontend/src/Utils/Footer.jsx
@@ -9,18 +9,18 @@ const Footer = () => {
     return (
         <div className='text-white bg-dark container'>
             <div className='text-white pt-2 d-flex align-items-start justify-content-between' style={{ display: '' }} data-aos="slide-right" data-aos-delay='100' data-aos-anchor-easing='ease-in-out' data-aos-duration='1000' data-aos-mirror='false'>
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://wa.me/+918005183363'><i class="bi bi-whatsapp"> Whatsapp</i> </Link>
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.instagram.com/slogfy?igsh=MXdraDhpbzlxaHk1Zg=='><i class="bi bi-instagram"> Instagram</i> </Link>
-                {/* <Link className='text-decoration-none text-white' target='__blank' to='https://Slogfy@gmail.com'><i class="bi fs-3 bi-envelope-at"></i></Link> */}
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.facebook.com/slogfy'><i class="bi bi-facebook"> Facebook</i> </Link>
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.linkedin.com/company/slogfy/'><i class="bi bi-linkedin"> LinkedIn</i> </Link>
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://x.com/slogfy?t=7W11e-jgtl8NoIiFlBllQQ&s=09'><i class="bi bi-twitter-x px-1"> Twitter</i> </Link>
-                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://youtube.com/@slogfy8260'><i class="bi bi-youtube"> YouTube</i>
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://wa.me/+918005183363'><b class="bi bi-whatsapp"> Whatsapp</b> </Link>
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.instagram.com/slogfy?igsh=MXdraDhpbzlxaHk1Zg=='><b class="bi bi-instagram"> Instagram</b> </Link>
+                {/* <Link className='text-decoration-none text-white' target='__blank' to='https://Slogfy@gmail.com'><b class="bi fs-3 bi-envelope-at"></b></Link> */}
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.facebook.com/slogfy'><b class="bi bi-facebook"> Facebook</b> </Link>
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://www.linkedin.com/company/slogfy/'><b class="bi bi-linkedin"> LinkedIn</b> </Link>
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://x.com/slogfy?t=7W11e-jgtl8NoIiFlBllQQ&s=09'><b class="bi bi-twitter-x px-1"> Twitter</b> </Link>
+                <Link className='hoveringEffect text-decoration-none text-white' target='__blank' to='https://youtube.com/@slogfy8260'><b class="bi bi-youtube"> YouTube</b>
                 </Link>
             </div>
             <hr />
             <Link to="https://maps.app.goo.gl/4eqS6DdxRkrgA5Rt5" target='__blank' className='text-decoration-none'>
-                <i class="bi fs-3 bi-globe-americas"></i> &nbsp;Address: F-76, near saket metro, Saidulajab, DDA Flats, Munirka, New Delhi, Delhi 110030
+                <b class="bi fs-3 bi-globe-americas"></b> &nbsp;Address: F-76, near saket metro, Saidulajab, DDA Flats, Munirka, New Delhi, Delhi 110030
             </Link>
             <hr />
             <div>
