35b04ac04c5ea881350feed50b4e9d622042dad4
anirudhkala110 <anirudhkala110@gmail.com>
2024-05-01 14:44:14 +0530

    Updating About page

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -1,47 +1,46 @@
 import React from 'react'
 import bbr from '../../Images/bbrn.jpg'
+import b1 from '../../Images/b1.jpeg'
+import b2 from '../../Images/b2.jpeg'
+import b3 from '../../Images/b3.jpeg'
+import b4 from '../../Images/b4.jpeg'
 import './AboutUs.css'
 
 const About = () => {
   return (
     <div className='min-vh-100'>
-      <div className='aboutUsBacki' style={{ fontSize: '78px' }}>
-        <center className='fw-bold aboutusName' style={{filter:'drop-shadow(12px 12px 2px black)'}}>About Us</center>
+      <div className='aboutUsBacki d-flex align-items-center justify-content-center' style={{ fontSize: '78px', height: '250px', width: '100%' }} >
+        <center className='fw-bold aboutusName' style={{ filter: 'drop-shadow(12px 12px 2px black)' }}>About Us</center>
       </div>
       <div className='container bg-black pt-3' style={{ borderLeft: '1px solid #8080806b', borderRight: '1px solid #8080806b' }}>
         <hr />
-        <div>
-          <div>
-            At RoboShop, we're pioneering the frontier of robotics, one arm at a time. Our platform serves as the ultimate hub for all things robotic arms, catering to enthusiasts, professionals, and businesses seeking innovation and automation solutions.
+        <div className='row'>
+          <img src={b3} className='rounded-3 col-sm-12 col-lg-6 col-xl-6 col-md-6' />
+          <div className='col-sm-12 col-lg-6 col-xl-6 col-md-6'>
+            <div className='row'>
+              <div className='col-12'>
+                <ul className='' style={{ listStyle: 'none' }}>
+                  <li>Slogfy is the brand created by Roboslog.</li>
+                  <li>Slogfy is an integral part of Roboslog.</li>
+                  <li>
+                    Slogfy assists people to secure their home with
+                    creating matter protocol based smart home automation
+                    that simply makes your smarter than ever and most precisely "SAFE".</li>
+                  <li>It's helping people since 2021 since it's establishment.</li>
+                </ul>
+                <img className='col-12 rounded-3' src={b2} style={{ maxHeight: "280px" }} />
+              </div>
+            </div>
           </div>
-          <br />
-          <div>
-            Founded on a passion for technological advancement and a drive for excellence, RoboShop is dedicated to democratizing access to cutting-edge robotic arm technology. We believe that robotics holds the key to transforming industries, driving efficiency, and unlocking new possibilities across various sectors.
-          </div>
-          <br />
-          <div>
-            RoboShop stands apart as a curator of premium robotic arm solutions, sourced from leading manufacturers and suppliers worldwide. Our carefully curated selection encompasses a diverse range of robotic arms, from industrial-grade machines for manufacturing to precision tools for research and experimentation.
-          </div>
-          <br />
-          <div>
-            What sets RoboShop apart is our commitment to empowerment and education. Beyond providing access to state-of-the-art technology, we aim to inspire and educate our community about the transformative potential of robotics. Through informative content, tutorials, and resources, we empower individuals and businesses to harness the power of robotics and drive innovation in their respective fields.
-          </div>
-          <br />
-          <div>
-            Customer satisfaction is at the heart of everything we do at RoboShop. Our dedicated team is here to provide personalized support, guidance, and assistance at every step of your robotic arm journey. Whether you're a seasoned professional or a curious newcomer, we're here to ensure that your experience with RoboShop is seamless, rewarding, and inspiring.
-          </div>
-          <br />
-          <div>
-            Join us at RoboShop and embark on a journey of discovery, innovation, and transformation. Explore our vast selection of robotic arms, connect with fellow enthusiasts, and unlock the endless possibilities of robotics with RoboShop today!
-          </div>
-          <br />
-
-
-
-
-
         </div>
       </div>
+      <br />
+      <br />
+      <div>
+
+      </div>
+      <br />
+      <br />
     </div>
   )
 }
