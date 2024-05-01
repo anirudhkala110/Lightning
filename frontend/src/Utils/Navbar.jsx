15c2cbebc62ffa121c07d462bd5f483232e9f602
anirudhkala110 <anirudhkala110@gmail.com>
2024-05-01 18:02:25 +0530

    Updating About page

--- frontend/src/Utils/Navbar.jsx
+++ frontend/src/Utils/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
     const admin = useContext(userContext);
     const navigate = useNavigate()
     const handleRoute = (route) => {
-        navigate(`/${route}`)
+        // navigate(`/${route}`)
+        window.location.href = `${route}`
+        // window.location.reload = `/${route}`
     }
     const handleCall = (number) => {
         const phoneNumber = parseInt(number); // Replace this with the phone number you want to call
@@ -42,12 +44,12 @@ const Navbar = () => {
                                 <i class="bi bi-youtube fs-4 "></i>&nbsp;&nbsp;
                             </Link>
                         </div> */}
-                        <div className='show1000px ms-4'>
+                        <div className='show1000px ms-4 '>
                             {/* <div className=''> */}
                             <div className='d-flex fw-semibold'>
                                 {/* <button className='fw-semibold text-white btn px-2 py-1 adminTag'>{admin.user}</button> */}
                                 <NavLink to='/'>
-                                    <button className="btn btnHover fw-bold navsupport "  >
+                                    <button className="btn btnHover fw-bold navsupport ">
                                         {/* <button className="btn btn-primary ms-2 " type="button" > */}
                                         Home
                                     </button>
@@ -65,7 +67,7 @@ const Navbar = () => {
                                     </button>
                                 </NavLink>
                                 <Link to='' >
-                                    <button className="btn btnHover fw-bold navsupport "  onClick={e=>alert('Available Soon..')}>
+                                    <button className="btn btnHover fw-bold navsupport " onClick={e => alert('Available Soon..')}>
                                         {/* <button className="btn btn-primary ms-2 " type="button" > */}
                                         App
                                     </button>
@@ -85,7 +87,7 @@ const Navbar = () => {
                             </strong>
                         </button>
                     </div>
-                    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                    <div className="offcanvas offcanvas-end " tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                         <div className="offcanvas-header bg-black">
                             <a href='/' className='text-decoration-none text-white d-flex ps-0 align-items-center' >
                                 <div className='bg-black text-white rounded-circle'>
@@ -97,9 +99,17 @@ const Navbar = () => {
                             <button type="button" className="btn-close text-reset text-white fs-3 fw-bolder py-2" data-bs-dismiss="offcanvas" aria-label="Close" style={{ color: 'white' }}><i class="bi bi-x-lg text-white"></i></button>
                         </div>
                         <hr />
-                        <div className="offcanvas-body " style={{ minHeight: "500px" }}>
-                            <NavLink to='/' className="btn  my-1 w-100 rounded-0 linkTabs" >Home</NavLink>
-                            <NavLink to='/about' className='btn  my-1 w-100 rounded-0 linkTabs'>About</NavLink>
+                        <div className="offcanvas-body  " style={{ minHeight: "500px" }}>
+                            <NavLink to='/home' className="btn  my-1 w-100 rounded-0 linkTabs" >
+                                <div onClick={e => handleRoute('/')} >
+                                    Home
+                                </div>
+                            </NavLink>
+                            <div onClick={e => handleRoute('/about')} >
+                                <NavLink to='/about' className='btn  my-1 w-100 rounded-0 linkTabs'>
+                                    About
+                                </NavLink>
+                            </div>
                             {/* <NavLink to='/contact-us' className='btn  my-1 w-100 rounded-0 linkTabs'>Contact</NavLink> */}
                             {/* <NavLink to='/all-products' className="btn  my-1 w-100 rounded-0 linkTabs" type="button" >Products</NavLink> */}
                             {/* <NavLink to='/services' className='btn  my-1 w-100 rounded-0 linkTabs'>Services</NavLink> */}
@@ -108,37 +118,55 @@ const Navbar = () => {
                             {/* <NavLink to='/login' className='w-50 btn  rounded-0 linkTabs'>Login</NavLink>
                             <NavLink to='/register' className='w-50 btn  rounded-0 linkTabs'>Sign Up</NavLink>
                             <NavLink to='/home' className='w-50 btn  rounded-0 linkTabs'>Logout</NavLink> */}
-                            <NavLink to='/support' className='btn  my-1 w-100 rounded-0 linkTabs'>Support</NavLink>
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://wa.me/+918005183363' data-aos="fade-up" data-aos-delay='150' data-aos-anchor-easing='ease-in-out' data-aos-duration='500' data-aos-mirror='true'>
-                                <i class="bi bi-whatsapp"></i> Whatsapp
-                            </Link>
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.instagram.com/slogfy?igsh=MXdraDhpbzlxaHk1Zg==' data-aos="fade-up" data-aos-delay='150' data-aos-anchor-easing='ease-in-out' data-aos-duration='500' data-aos-mirror='true'>
-                                <i class="bi bi-instagram"></i> Instagram
-                            </Link>
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://Slogfy@gmail.com' data-aos="fade-up" data-aos-delay='150' data-aos-anchor-easing='ease-in-out' data-aos-duration='500' data-aos-mirror='true'>
-                                <i class="bi bi-envelope-at"></i> Gmail
-                            </Link>
+                            <div onClick={e => handleRoute('/support')} >
+                                <NavLink to='/support' className='btn  my-1 w-100 rounded-0 linkTabs'>
+                                    Support
+                                </NavLink>
+                            </div>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://wa.me/+918005183363' >
+                                    <i class="bi bi-whatsapp"></i> Whatsapp
+                                </NavLink>
+                            </div>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.instagram.com/slogfy?igsh=MXdraDhpbzlxaHk1Zg==' >
+                                    <i class="bi bi-instagram"></i> Instagram
+                                </NavLink>
+                            </div>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://Slogfy@gmail.com' >
+                                    <i class="bi bi-envelope-at"></i> Gmail
+                                </NavLink>
+                            </div>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.facebook.com/slogfy' >
+                                    <i class="bi bi-facebook"></i> Facebook
+                                </NavLink>
+                            </div>
 
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.facebook.com/slogfy' data-aos="fade-up" data-aos-delay='150' data-aos-anchor-easing='ease-in-out' data-aos-duration='500' data-aos-mirror='true'>
-                                <i class="bi bi-facebook"></i> Facebook
-                            </Link>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.linkedin.com/company/slogfy/' >
+                                    <i class="bi bi-linkedin"></i> LinkedIn
+                                </NavLink>
+                            </div>
 
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://www.linkedin.com/company/slogfy/' data-aos="fade-up" data-aos-delay='150' data-aos-anchor-easing='ease-in-out' data-aos-duration='500' data-aos-mirror='true'>
-                                <i class="bi bi-linkedin"></i> LinkedIn
-                            </Link>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://x.com/slogfy?t=7W11e-jgtl8NoIiFlBllQQ&s=09'>
+                                    <i class="bi bi-twitter-x"></i> Twiter
+                                </NavLink>
+                            </div>
 
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://x.com/slogfy?t=7W11e-jgtl8NoIiFlBllQQ&s=09'>
-                                <i class="bi bi-twitter-x"></i> Twiter
-                            </Link>
+                            <div className='bg-black text-white'>
+                                <NavLink className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://youtube.com/@slogfy8260'>
+                                    <i class="bi bi-youtube"></i> YouTube
+                                </NavLink>
+                            </div>
+                            <div className='bg-black text-white'>
+                                <NavLink to="https://maps.app.goo.gl/4eqS6DdxRkrgA5Rt5" className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' >
 
-                            <Link className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' to='https://youtube.com/@slogfy8260'>
-                                <i class="bi bi-youtube"></i> YouTube
-                            </Link>
-
-                            <Link to="https://maps.app.goo.gl/4eqS6DdxRkrgA5Rt5" className='btn  my-1 w-100 rounded-0 linkTabs' target='__blank' >
-
-                                <i class="bi bi-globe-americas"></i> &nbsp;Address
-                            </Link>
+                                    <i class="bi bi-globe-americas"></i> &nbsp;Address
+                                </NavLink>
+                            </div>
                         </div>
                     </div>
                 </div>
