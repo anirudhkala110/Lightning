6973fd76d4e45f6346504e627a5b26f4d028e132
anirudhkala110 <anirudhkala110@gmail.com>
2024-05-01 11:55:55 +0530

    Updated Data for footer

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Logo from '../../Images/frst.png'
@@ -12,11 +12,57 @@ import voiceAssistant from '../../Images/voiceAssistant.jpg'
 import homeAutomationUsingMotionMatterProtocol from '../../Images/homeAutomationUsingMotionMatterProtocol.jpg'
 import Typewriter from '../../Utils/Typewriter';
 import AutomaticImageSlider from '../../Images/AutomaticImageSlider';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import FAQ from './FAQ';
-
+import emailjs from '@emailjs/browser';
+import { Helmet } from 'react-helmet';
+import axios from 'axios'
 
 const Home = () => {
+    const navigate = useNavigate();
+    const form = useRef();
+    const [name, setName] = useState()
+    const [mobile, setMobile] = useState()
+    const [email, setEmail] = useState()
+    const [query, setQuery] = useState()
+    const [finalquery, setFQuery] = useState()
+    const [date, setDate] = useState()
+    const [time, setTime] = useState()
+    const [id, setId] = useState()
+    const currentDate = new Date().toLocaleDateString();
+    const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const handleSubmit = (e) => {
+        setDate(currentDate)
+        setTime(currentTime)
+        setId(Date.now())
+        e.preventDefault();
+        if (name === '' || !name || mobile === '' || !mobile || email === '' || !email || query === '' || !query) {
+            alert("Please Enter All the Fields")
+        }
+        else {
+            axios.post('https://localhost:8099/api/saveData', { name: name, mobile: mobile, email: email, query: query, date: date, time: time, id: id })
+                //   axios.post('https://api.legalbrother.in/api/saveData', { name: name, mobile: mobile, email: email, query: query, date: date, time: time, id: id })
+                .then(res => {
+                    console.log(res)
+                    if (res.data.success) {
+                        alert("Your  Query Has been saved.\nNow you are redirecting to the Homepage. . .")
+                        window.location.href = '/'
+                    }
+                    else {
+                        alert("Sorry, not able to send your query.\nTry again after some time.")
+                    }
+                })
+                .catch(err => console.log(err))
+            emailjs.sendForm('service_j5cbb74', 'template_x4fuopb', form.current, 'A7Zgb_4LO8WMThAjR')
+                .then((result) => {
+                    alert("Your  message ✉ Has been sent.\nNow you are redirecting to the Homepage. . .")
+                    navigate('/')
+                }, (error) => {
+                    alert("Sorry, not able to send your query.\nTry again after some time.")
+                    console.log(error.text);
+                });
+        }
+    }
     useEffect(() => {
         AOS.init({ duration: 200 })
     }, [])
@@ -27,6 +73,15 @@ const Home = () => {
     const handleGmailButtonClick = (recipientEmail) => {
         window.location.href = `mailto:${recipientEmail}`;
     };
+    const handleQuery = (e) => {
+        setQuery(e.target.value)
+        setDate(currentDate)
+        setTime(currentTime)
+        setFQuery(name + "\n" + email + "\n" + mobile + "\n" + date + "\n" + time + "\n" + 'Message is : ' + e.target.value)
+    }
+    const handleRoute = (route) => {
+        navigate(`/${route}`)
+    }
     function truncate(str, size) {
         var updatedStr = ''
         if (str.length > size) {
@@ -249,6 +304,43 @@ const Home = () => {
                         ASKED QUESTIONS
                     </center>
                     <FAQ />
+                </div>
+                <center className='fs-3 fw-semibold'>Have a Question ? Ask Here !</center>
+                <div className='row'>
+                    <div className="container pt-3 col-lg-6 col-xl-6 my-2 py-2 col-sm-12 pb-1">
+                        <form onSubmit={(e) => handleSubmit(e)} ref={form} >
+                            <div className="form-group text-secondary fw-bolder">
+                                <label className="fw-bold" htmlFor="formGroupName">Name <sup className='text-danger'>*</sup></label>
+                                <input type="text" className="form-control   text-light rounded-3 border-0  bg-dark" id="formGroupName" placeholder="Your Valid Name" name="user_name" value={name} onChange={e => setName(e.target.value)} required />
+                            </div>
+                            <div className="form-group text-secondary fw-bolder">
+                                <label className="fw-bold" htmlFor="formGroupMobile">Mobile Number<sup className='text-danger'>*</sup></label>
+                                <input type="number" className="form-control   text-light rounded-3 border-0  bg-dark" id="formGroupMobile" placeholder="Mobile Number" name="mobile" value={mobile} onChange={e => setMobile(e.target.value)} required />
+                            </div>
+                            <div className="form-group text-secondary fw-bolder">
+                                <label className="fw-bold" htmlFor="formGroupEmail">Email Address<sup className='text-danger'>*</sup></label>
+                                <input type="email" className="form-control   text-light rounded-3 border-0  bg-dark" id="formGroupEmail" placeholder="Email Address" name="user_email" value={email} onChange={e => setEmail(e.target.value)} required />
+                            </div>
+                            <div className="form-group text-secondary fw-bolder mb-2">
+                                <label className="fw-bold" htmlFor="formGroupQuery">Messege<sup className='text-danger'>*</sup></label>
+                                <textarea
+                                    type="text"
+                                    className="form-control   text-light rounded-3 border-0  bg-dark"
+                                    cols={30}
+                                    rows={5}
+                                    style={{ resize: "none" }}
+                                    id="formGroupQuery"
+                                    value={query}
+                                    onChange={e => handleQuery(e)}
+                                    placeholder={`✉ Enter your Message here`} required
+                                />
+                                <textarea name="message" value={finalquery} style={{ display: "none" }} />
+                            </div>
+                            <button type='submit' className="btn border-0 rounded-0 mb-2 fw-bolder send-mail btn-primary w-100" value="Send">Send Mail</button>
+                            {/* <div className="btn btn-success border-0 rounded-0 ms-2 mb-2 call-btn" id="call" onClick={() => handleCall(8005183363)}>Make a Call</div> */}
+                            <div className="btn btn-warning border-0 w-100 rounded-0 fw-bolder mb-2 call-btn" id="call" onClick={() => handleRoute('home')}>Home</div>
+                        </form>
+                    </div>
                 </div>
                 {/* <hr /> */}
                 {/* <div className='container'>
